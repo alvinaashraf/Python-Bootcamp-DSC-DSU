9bee9c586de54d2188a37b24e044baa93074b7fe
alvinaashraf <57407049+alvinaashraf@users.noreply.github.com>
2020-12-10 18:40:34 +0500

    Create assignment.py

---
+++ week_1/assignment.py
@@ -0,0 +1,16 @@
+def string():
+    print('''
+          h
+            e 
+              l
+                 l 
+                    o
+                        w 
+                            o 
+                                r 
+                                    l 
+                                        d 
+          
+          ''')
+    
+string()
